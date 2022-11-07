3f9c0005d53c7511db1f2bc4c8b8be59a01becac
KMAN-THECHEQUEBOOK <katlehomoeletsi20011@gmail.com>
2022-11-07 15:00:47 +0200

    Create NavyUniform.cpp

---
+++ NavyUniform.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <vector>
+#include <sstream>
+using namespace std;
+#include "NavyUniform.h"
+
+   NavyUniform:: NavyUniform():Uniform(){
+      name="NavyUniform";
+   
+
+   
+   }
+    string NavyUniform:: getName(){
+    return name;
+ }
+ 
