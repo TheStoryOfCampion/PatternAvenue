ef50bba406fa6e838b353110008c13eba744603c
KMAN-THECHEQUEBOOK <katlehomoeletsi20011@gmail.com>
2022-11-08 03:19:14 +0200

    Create creatingWeapon.h

---
+++ creatingWeapon.h
@@ -0,0 +1,19 @@
+#ifndef CREATINGWEAPON_H
+#define CREATINGWEAPON_H
+using namespace std;
+#include "ConcreteGuns.h"
+class creatingWeapon{
+
+public:
+   
+   void helpus(ConcreteGuns*);
+
+private:
+
+    Guns* g;
+
+
+
+
+};
+#endif
